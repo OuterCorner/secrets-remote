c65c7a7feed6d94a55164b4fc0270354b528490d
Paulo Andrade <paulo@outercorner.com>
2020-01-20 22:03:58 +0000

    Test now running again.

--- test/pairing-tests.js
+++ test/pairing-tests.js
@@ -1,29 +1,42 @@
 const assert = require('chai').assert
-const { pairDevice } = require('../lib')
+const { pairDevice, getNoiseLib } = require('../lib')
 const startMockChatServer = require('./mock-chat-server')
 
+const chatServerPort = 8085
+
 describe('Pairing', function() {
-    var chatServer = undefined
-    const chatServerPort = 8085
-    before(function(done) {
-        chatServer = startMockChatServer(chatServerPort)
-        done()
+        
+    before(async function() {
+        this.chatServer = startMockChatServer(chatServerPort)
+        const noise = await getNoiseLib()
+        const keys = noise.CreateKeyPair(noise.constants.NOISE_DH_CURVE25519)
+        this.serverStaticKeyPair = {
+            pub: keys[1],
+            priv: keys[0]
+        }
     })
 
     after(function(done) {
-        chatServer.close(done)
+        this.chatServer.close(done)
     })
 
     describe('#pairDevice()', function() {
         it('Should return device on successful pairing', function(done) {
-            
-            pairDevice(`ws://localhost:${chatServerPort}`, (pairingInfo) => {
-                assert.typeOf(pairingInfo.url, 'string')
-            })
-            .then(device => {
+            try {
+                pairDevice(`ws://localhost:${chatServerPort}`, this.serverStaticKeyPair, (pairingInfo) => {
+                assert.typeOf(pairingInfo.peerId, 'string')
+                assert.typeOf(pairingInfo.secret, 'string')
+                assert.instanceOf(pairingInfo.url, URL)
+                assert.equal(pairingInfo.url.protocol, 'secrets:')
+                assert.equal(pairingInfo.url.searchParams.get('pairing-secret'), pairingInfo.secret)
+                assert.equal(pairingInfo.url.searchParams.get('requester-id'), pairingInfo.peerId)
+            }).then(device => {
                 assert.isNotNull(device)
                 done()
             })
+            } catch (error) {
+                console.error(error)    
+            }
         });
     });
 });
