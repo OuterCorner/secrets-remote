e7814bc53989d21f6cdff03473440c2da9d0bc4c
Paulo F. Andrade <paulo@outercorner.com>
2020-11-04 13:05:10 +0000

    Added support for ad hoc secret requests

--- bin/cli-request.js
+++ bin/cli-request.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const program = require('commander')
 const chalk = require('chalk')
-const { requestSecret, PushNotificationService } = require('../lib')
+const clear = require('clear')
+const qrcode = require('qrcode-terminal')
+const { requestSecret, PushNotificationService, requestAdHocSecret } = require('../lib')
 const { getStaticKeyPair, store } = require('./cli-common')
 const pushService = require('superagent-use')(require('superagent'))
 const superagent_prefix = require('superagent-prefix')
@@ -19,13 +21,24 @@ program
     .option('-u, --url <url>', 'url of associated service', (url) => new URL(url))
     .option('-t, --type <type>', `type of item [${validTypes.join('|')}]`, parseType, {type: 'login'})
     .option('-d, --device <device>', 'name or index of device to query (all devices are queried by default)', parseDevice)
+    .option('-a, --adhoc', 'perform an adhoc request, if set \'-d\' is ignored')
+    .option('-Q, --large-qrcode', 'use a large QR Code (only applicable on ad hoc requests)', false)
     .description("Remotely request a secret from a paired device")
     .action( function(searchString) {
-        const devices = this.device || store.get('devices') || []
-        if (devices.length == 0) {
-            throw new Error("No paired devices.")
+        var opts = {}
+        opts.url = this.url
+        opts.item = this.type
+        if (this.adhoc) {
+            opts.adhoc = this.adhoc
+            opts.largeQrCode = this.largeQrCode
+        } else {
+            const devices = this.device || store.get('devices') || []
+            if (devices.length == 0) {
+                throw new Error("No paired devices.")
+            }
+            opts.devices = devices
         }
-        request(searchString, this.url, this.type, devices)
+        request(searchString, opts)
     })
 
 program.parse(process.argv)
@@ -77,22 +90,44 @@ function parseDevice(deviceArg, selectedDevices = []) {
 }
 
 
-async function request(searchString, url, item, devices) {
+async function request(searchString, options) {
+    let url = options.url
+    let item = options.item
+    let devices = options.devices || []
+    let adhoc = options.adhoc || false
+    let largeQrCode = options.largeQrCode || false
+
     try {
+        const charServiceAddr = 'wss://chat.outercorner.com/v1/'
         const staticKeyPair = await getStaticKeyPair()
 
         const query = { searchString, url, item }
-        const pns = new PushNotificationService(pushService)
-        pns.on("pushed", (device) => {
-            console.error(chalk.green('✓') + ` Notified device "${device.name}".`)
-        })
-        pns.on("error", (error) => {
-            console.error(chalk.red("✕") + ` Failed to notify device "${error.device.name}": ${error}`)
-        })
+        if (adhoc) {
+            const { secret } = await requestAdHocSecret(charServiceAddr, query, (requestInfo) => {
+                const requestUrl = requestInfo.url.href
+                clear()
+                qrcode.generate(requestUrl, {small: !largeQrCode}, (output) => {
+                    console.error(output)
+                })
+                console.error(requestUrl)
+                console.error("Scan this QR code to fulfill this request with Secrets.")
+            })
 
-        const { secret } = await requestSecret('wss://chat.outercorner.com/v1/', staticKeyPair, pns, devices, query)
+            console.log(JSON.stringify(secret))
+        }
+        else {
+            const pns = new PushNotificationService(pushService)
+            pns.on("pushed", (device) => {
+                console.error(chalk.green('✓') + ` Notified device "${device.name}".`)
+            })
+            pns.on("error", (error) => {
+                console.error(chalk.red("✕") + ` Failed to notify device "${error.device.name}": ${error}`)
+            })
+
+            const { secret } = await requestSecret(charServiceAddr, staticKeyPair, pns, devices, query)
         
-        console.log(JSON.stringify(secret))
+            console.log(JSON.stringify(secret))
+        }
 
     } catch(e) {
         console.log(chalk.red(e.stack))
